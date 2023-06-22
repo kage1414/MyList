9ed772afe9c3c6716cdd985a06f9bb68c5718bb0
kage1414 <kylejohnson92294@gmail.com>
2023-06-22 10:55:39 -0400

    Connect postgres

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+
+	"MyList/ent"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	client, err := ent.Open("postgres",
+		fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+			os.Getenv("PG_HOST"),
+			os.Getenv("PG_PORT"),
+			os.Getenv("PG_USER"),
+			os.Getenv("PG_DBNAME"),
+			os.Getenv("PG_PASS"),
+		),
+	)
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
 	}
 }
