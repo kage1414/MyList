541e8177f53cbbe485afb6c302f6b5bc8582a31e
kage1414 <kylejohnson92294@gmail.com>
2023-06-22 15:49:08 -0400

    Add base ui

--- rest/main.go
+++ rest/main.go
@@ -5,11 +5,15 @@ import (
 
 	"MyList/ent"
 
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(client *ent.Client, ctx context.Context) {
 	r := gin.Default()
+
+	r.Use(static.Serve("/", static.LocalFile("./ui/dist", true)))
+
 	api := r.Group("/api")
 	i := Item{"/item", client, ctx}
 	is := Items{"/items", client, ctx}
